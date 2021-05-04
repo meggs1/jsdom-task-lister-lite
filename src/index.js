d0da53278202b30e51bcb0855d15e28e3843789a
megan <megan.schertz@gmail.com>
2021-05-03 23:47:38 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  const taskList = document.querySelector('#tasks') 
+  const taskForm = document.querySelector('#create-task-form')
+  const newTaskDescription = document.querySelector('#new-task-description')
+
+  taskForm.addEventListener('submit', function(event) {
+    event.preventDefault();
+    const newLi = document.createElement('li');
+    newLi.textContent = newTaskDescription.value;
+    taskList.appendChild(newLi);
+  });
 });
